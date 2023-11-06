63cf93b86cd6fa6823f99d2dd63ce2597c8baac2
K3v1nNZ <icegamer37@gmail.com>
2023-11-07 10:28:40 +1300

    added date, color, and footer to the embed

--- GRSDiscordBotMain/main.py
+++ GRSDiscordBotMain/main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="../.env")
@@ -11,7 +12,7 @@ async def on_ready():
     print(f"Bot is ready as {client.user}".format(client))
 
 @client.command()
-async def send_embed(ctx: discord.ApplicationContext, title: discord.Option(str, "The title of the embed", required=True), description: discord.Option(str, "The description of the embed", required=True)):
+async def send_embed(ctx: discord.ApplicationContext, title: discord.Option(str, "The title of the embed", required=True), description: discord.Option(str, "The description of the embed", required=True), color: discord.Option(str, "The hex color of the embed", required=False)):
     if not ctx.guild_id in [855327322618331157, 782368687419817995]:
         await ctx.respond("This command can only be used in the GRS Discord", ephemeral=True)
         return
@@ -19,7 +20,11 @@ async def send_embed(ctx: discord.ApplicationContext, title: discord.Option(str,
         await ctx.respond("You do not have permission to use this command", ephemeral=True)
         return
     description = description.replace("\\n", "\n")
-    embed = discord.Embed(title=title, description=f"{description}", color=discord.Color.red())
+    embed = discord.Embed(title=title, description=f"{description}").set_footer(text="GRS - " + datetime.datetime.now().strftime("%d/%m/%Y"))
+    if color:
+        embed.color = int(color, 16)
+    else:
+        embed.color = discord.Color.red()
     await ctx.send(embed=embed)
     await ctx.respond("Embed sent successfully", ephemeral=True)
 
